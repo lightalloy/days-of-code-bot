9253f8edc07bcc4582268fab23a039d2e2153f79
lightalloy <lightallloy@gmail.com>
2019-06-08 20:09:49 +0300

    Added ability to set up proxy if needed

--- boot.rb
+++ boot.rb
@@ -8,3 +8,10 @@ require_relative "lib/relations/challenge_comments"
 require_relative 'lib/register_user'
 require_relative 'lib/save_comment'
 require_relative 'lib/rom_container'
+require_relative 'lib/days_of_code_bot/faraday_adapter'
+
+Faraday::Adapter.register_middleware days_of_code_bot: DaysOfCodeBot::FaradayAdapter
+
+Telegram::Bot.configure do |config|
+  config.adapter = :days_of_code_bot
+end
---
+++ lib/days_of_code_bot/faraday_adapter.rb
@@ -0,0 +1,22 @@
+module DaysOfCodeBot
+  class FaradayAdapter < Faraday::Adapter::NetHttp
+    def net_http_connection(env)
+      client = if proxy?
+                Net::HTTP::Proxy(proxy_options[:server], proxy_options[:port])
+              else
+                Net::HTTP
+              end
+      client.new(env[:url].hostname, env[:url].port || (env[:url].scheme == 'https' ? 443 : 80))
+    end
+
+    private
+
+    def proxy_options
+      @proxy_options ||= { server: ENV['PROXY_SERVER'], port: ENV['PROXY_PORT'] }
+    end
+
+    def proxy?
+      !ENV["PROXY_SERVER"].to_s.empty?
+    end
+  end
+end
